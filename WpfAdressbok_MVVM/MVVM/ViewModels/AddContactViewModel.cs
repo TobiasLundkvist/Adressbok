834549f536801a9919bdd6f31a1bd19791369d1e
Tobias Lundkvist <tobias_lundkvist@hotmail.com>
2023-01-19 17:17:27 +0100

    Navigering mellan lista och lägg till added

--- WpfAdressbok_MVVM/MVVM/ViewModels/AddContactViewModel.cs
+++ WpfAdressbok_MVVM/MVVM/ViewModels/AddContactViewModel.cs
@@ -1,13 +1,47 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using WpfAdressbok_MVVM.MVVM.Models;
 namespace WpfAdressbok_MVVM.MVVM.ViewModels
 {
-    public class AddContactViewModel : ObservableObject
+    public partial class AddContactViewModel : ObservableObject
     {
+
+
+        [ObservableProperty]
+        private string title = "Skapa kontakt";
+
+        [ObservableProperty]
+        private string firstName = string.Empty;
+
+        [ObservableProperty]
+        private string lastName = null!;
+
+        [ObservableProperty]
+        private string email = null!;
+
+        [ObservableProperty]
+        private string phoneNumber = null!;
+
+        [ObservableProperty]
+        private string address = null!;
+
+        [ObservableProperty]
+        private string postalCode = null!;
+
+        [ObservableProperty]
+        private string city = null!;
+
+        [RelayCommand]
+        private void Add()
+        {
+
+        }
+
     }
 }
